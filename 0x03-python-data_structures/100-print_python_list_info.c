ce61739610477dcab335e44686c6c99c5b247321
Mahmoud Salah <ms685116#gmail.com>
2023-08-13 15:03:22 +0300

    cpython

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
 #include <Python.h>
-
+#include <stdio.h>
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: pointer to PyObject
+ * Return: void
+ */
+*/
 void print_python_list_info(PyObject *p)
 {
 	int i;
