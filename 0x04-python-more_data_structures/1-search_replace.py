c830f5e78c08737ca13ab5e1b1c6e9c472979ff4
Mahmoud Salah <ms685116@gmail.com>
2023-08-16 13:42:58 +0300

    replace element by another

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    return list(map(lambda x: replace if x == search else x, my_list))
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
