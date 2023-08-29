fa5d94f01e6e17cd222fb20c5b78b14adfb22a23
Mahmoud Salah <ms685116@gmail.com>
2023-08-29 20:48:58 +0300

    area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script for square class"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """constructor method
+        Parameters:
+            size (int): size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """method to calculate square area
+        Returns:
+            square area
+        """
+        return self.__size * self.__size
