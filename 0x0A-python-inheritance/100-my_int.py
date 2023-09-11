4ddb0b3783109c0cf792c56718dd39e89e2787f3
Mahmoud Salah <ms685116@gmail.com>
2023-09-11 23:26:48 +0300

    operator overloading

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""operator overloading"""
+
+
+class MyInt(int):
+    """subclass of int"""
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __eq__(self, obj):
+        """equality overloading"""
+        return self.value != obj
+    def __ne__(self, obj):
+        """inequality overloading"""
+        return self.value == obj
