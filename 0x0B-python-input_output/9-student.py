13992b9012d2671e6b456d566a9767561da35c68
Mahmoud Salah <ms685116@gmail.com>
2023-09-12 23:01:44 +0300

    to json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""student class module"""
+
+
+class Student:
+    """student class"""
+    def __init__(self,first_name,last_name,age):
+        """initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """object to json"""
+        return self.__dict__
