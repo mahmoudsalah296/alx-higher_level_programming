6bbd2e01a75de118a75dbc12ee31d38df4648589
Mahmoud Salah <ms685116@gmail.com>
2023-09-13 19:20:43 +0300

    append after specific text

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,7 +4,7 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """append after specific text method"""
-    with open(filename, "a+", encoding="utf-8") as f:
+    with open(filename, mode="r+", encoding="utf-8") as f:
         for line in f:
             if search_string in line:
                 f.write(new_string)
