637a6ac929567eb8193cf83390541e8d60454f4e
Mahmoud Salah <ms685116@gmail.com>
2023-09-13 19:04:54 +0300

    append after specific text

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""append after specific text"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append after specific text method"""
+    with open(filename, "a+", encoding="utf-8") as f:
+        for line in f:
+            if search_string in line:
+                f.write(new_string)
