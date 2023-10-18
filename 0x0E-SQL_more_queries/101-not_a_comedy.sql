8eac325f714760785d735e735a06c557c24624e7
Mahmoud Salah <ms685116@gmail.com>
2023-10-18 22:01:28 +0300

    not a comedy movie

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,14 @@
+-- a script that lists all shows without the genre Comedy in the database
+SELECT title
+FROM tv_shows
+WHERE title NOT IN
+(
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY tv_shows.title ASC;
