711b12d9616a9ab49c8516ab40d5ac81bc1d5cb3
Mahmoud Salah <ms685116@gmail.com>
2023-10-18 14:50:20 +0300

    not null attribute

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- a script that creates the table force_name
+-- he database name will be passed as an argument of the mysql command
+CREATE TABLE force_name(
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
