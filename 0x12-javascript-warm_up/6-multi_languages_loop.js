0d74af6cf2dc93a7ea0a934d6cc5b65963243af9
Mahmoud Salah <ms685116@gmail.com>
2023-11-13 14:36:02 +0200

    for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * a script that prints 3 lines using an array of string and a loop
+ */
+const arr = ['C is fun',
+  'Python is cool',
+  'JavaScript is amazing'];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
