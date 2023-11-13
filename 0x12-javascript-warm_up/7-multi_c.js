e6efc4da434963924c3aa417cdb54d25095dee59
Mahmoud Salah <ms685116@gmail.com>
2023-11-13 14:51:25 +0200

    print argv n times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/**
+ * a script that prints x times “C is fun”
+ */
+const process = require('process');
+arg = Number.parseInt(process.argv[2]);
+
+if (!process.argv[2]) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < arg) {
+    console.log('C is fun');
+    i++;
+  }
+}
