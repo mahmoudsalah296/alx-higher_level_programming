dc46fd6049509812153fccd909106a8904754418
Mahmoud Salah <ms685116@gmail.com>
2023-11-14 19:51:40 +0200

    dict

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/user/bin/node
 
-const dict = require('./101-data').dict;
+const dict = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
