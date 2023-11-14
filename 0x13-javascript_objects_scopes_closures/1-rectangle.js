5286bdf0c647c143cf21216a9936aaf41e6c361b
Mahmoud Salah <ms685116@gmail.com>
2023-11-14 16:03:03 +0200

    add attributes to the class

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* class rectangle with atrributes */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
