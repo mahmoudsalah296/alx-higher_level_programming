5988645af83cf118220f3db8cc417516f4d03563
Mahmoud Salah <ms685116@gmail.com>
2023-11-30 21:23:39 +0200

    find the peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,31 +2,24 @@
 """peack finder"""
 
 
-def find_peack_helper(array, lower, upper):
-    """helper function"""
-    mid = (lower + upper) // 2
-
-    if array[mid] > array[mid - 1] and array[mid] > array[mid + 1]:
-        return array[mid]
-    if array[mid + 1] > array[mid]:
-        return find_peack_helper(array, mid, upper)
-    else:
-        return find_peack_helper(array, lower, mid)
-
-
 def find_peak(list_of_integers):
     """find the peak in a list"""
-    lower = 0
-    upper = len(list_of_integers) - 1
-
-    if len(list_of_integers) == 0:
+    if list_of_integers is None or list_of_integers == []:
         return None
-    if len(list_of_integers) == 1:
+    lower = 0
+    upper = len(list_of_integers)
+    mid = ((upper - lower) // 2) + lower
+    mid = int(mid)
+    if upper == 1:
         return list_of_integers[0]
-    if len(list_of_integers) == 2:
-        return (
-            list_of_integers[0]
-            if list_of_integers[0] > list_of_integers[1]
-            else list_of_integers[1]
-        )
-    return find_peack_helper(list_of_integers, lower, upper)
+    if upper == 2:
+        return max(list_of_integers)
+    if (
+        list_of_integers[mid] >= list_of_integers[mid - 1]
+        and list_of_integers[mid] >= list_of_integers[mid + 1]
+    ):
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
