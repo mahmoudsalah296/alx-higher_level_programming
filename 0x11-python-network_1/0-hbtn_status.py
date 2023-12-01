b5c69119612f486374a034a1d78c93791c5ec1e8
Mahmoud Salah <ms685116@gmail.com>
2023-12-01 14:34:22 +0200

    first task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,10 @@
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
-    body = resp.read()
-    print("Body response:")
-    print(f"\t- type: {type(body)}")
-    print(f"\t- content: {body}")
-    print(f"\t- utf-8 content: {body.decode('UTF-8')}")
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
+        body = resp.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf-8 content: {body.decode('UTF-8')}")
