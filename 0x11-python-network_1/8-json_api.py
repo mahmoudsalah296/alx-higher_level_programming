10d42396131e5d9b3571d5cf0c93f77f20d400a3
Mahmoud Salah <ms685116@gmail.com>
2024-01-02 16:25:47 +0200

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print(f'[{response_json["id"]}] {response_json["name"]}')
-    except:
+    except Exception:
         print("Not a valid JSON")
