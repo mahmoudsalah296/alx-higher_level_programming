4a822d78a494505ffbc7f532cd78820c12266806
Mahmoud Salah <ms685116@gmail.com>
2024-01-05 17:11:12 +0200

    initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+conn = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3],
+    charset="utf-8",
+)
+
+cursor = conn.cursor()
+cursor.execute("select * from states")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+cursor.close()
+conn.close()
