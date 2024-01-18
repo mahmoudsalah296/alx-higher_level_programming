6ecb7bbbb87952136cc4ed6225f223710be6aa27
Mahmoud Salah <ms685116@gmail.com>
2024-01-18 13:40:20 +0200

    states names

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cursor.execute(
         """
         SELECT * FROM states
-        WHERE states.name LIKE "N%"
+        WHERE states.name LIKE BINARY "N%"
         ORDER BY states.id
     """
     )
